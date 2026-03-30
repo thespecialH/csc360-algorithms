bded7fd40533fb0dbb7b6ee4d08e415e5f74beb1
John Colton <jlh.colton@gmail.com>
2026-03-30 10:00:38 -0500

    binary search pseudocode finished

--- binary-search/pseudocode.c
+++ binary-search/pseudocode.c
@@ -6,7 +6,12 @@ mid_value = x/2; //this will need to be an integer
 
 while (search_term != array[mid_value])
 {
-	if (search_term < 
+	if (search_term < array[mid_value]) {
+		mid_value = (mid_value)/2;
+	} else {
+		mid_value = mid_value + (x - mid_value)/2;
+	}
+
 }
 
 return mid_value;
